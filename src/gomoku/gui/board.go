02c9bc6f3c6db559cbcd87d6ba9bce1a155d6910
Klemen Sever <ksever@student.42.fr>
2015-05-19 18:23:34 +0200

    Gomoku board displays pawns

--- src/gomoku/gui/board.go
+++ src/gomoku/gui/board.go
@@ -8,7 +8,7 @@ import (
 
 type Board struct {
 	Background *Texture
-	Prop       *Texture
+	Table      *Texture
 	WhitePawn  *Texture
 	BlackPawn  *Texture
 }
@@ -16,7 +16,7 @@ type Board struct {
 func NewBoard() *Board {
 	board := &Board{
 		Background: GetTextureFromImage("data/img/bg.jpg"),
-		Prop:       GetTextureFromImage("data/img/board.png"),
+		Table:      GetTextureFromImage("data/img/board.png"),
 		WhitePawn:  GetTextureFromImage("data/img/white.png"),
 		BlackPawn:  GetTextureFromImage("data/img/black.png"),
 	}
@@ -25,20 +25,22 @@ func NewBoard() *Board {
 	var finalW int32
 	var finalH int32
 
-	if board.Prop.size.W > DisplayMode.W {
-		ratio = float64(DisplayMode.W) / float64(board.Prop.size.W)
-		finalW = int32(float64(board.Prop.size.W) * ratio)
-		finalH = int32(float64(board.Prop.size.H) * ratio)
+	if board.Table.size.W > DisplayMode.W {
+		ratio = float64(DisplayMode.W) / float64(board.Table.size.W)
+		finalW = int32(float64(board.Table.size.W) * ratio)
+		finalH = int32(float64(board.Table.size.H) * ratio)
 	}
 
-	if board.Prop.size.H > DisplayMode.H {
-		ratio = float64(DisplayMode.H) / float64(board.Prop.size.H)
-		finalW = int32(float64(board.Prop.size.W) * ratio)
-		finalH = int32(float64(board.Prop.size.H) * ratio)
+	if board.Table.size.H > DisplayMode.H {
+		ratio = float64(DisplayMode.H) / float64(board.Table.size.H)
+		finalW = int32(float64(board.Table.size.W) * ratio)
+		finalH = int32(float64(board.Table.size.H) * ratio)
 	}
 
 	board.Background.pos = sdl.Rect{X: 0, Y: 0, W: DisplayMode.W, H: DisplayMode.H}
-	board.Prop.pos = sdl.Rect{X: DisplayMode.W/2 - finalW/2, Y: 0, W: finalW, H: finalH}
+	board.Table.pos = sdl.Rect{X: DisplayMode.W/2 - finalW/2, Y: 0, W: finalW, H: finalH}
+	board.WhitePawn.pos = sdl.Rect{X: 0, Y: 0, W: board.Table.pos.W / 19, H: board.Table.pos.H / 19}
+	board.BlackPawn.pos = sdl.Rect{X: 0, Y: 0, W: board.Table.pos.W / 19, H: board.Table.pos.H / 19}
 	return board
 }
 
@@ -56,13 +58,38 @@ func (b *Board) PlayScene() {
 	Renderer.Clear()
 
 	Renderer.Copy(b.Background.texture, &b.Background.size, &b.Background.pos)
-	Renderer.Copy(b.Prop.texture, &b.Prop.size, &b.Prop.pos)
+	Renderer.Copy(b.Table.texture, &b.Table.size, &b.Table.pos)
 
 	// Display content of board in top of background
-	demoArena := arena.Gomoku.Arena
-	for _ = range demoArena.Goban {
-		Renderer.Copy(b.WhitePawn.texture, &b.WhitePawn.size, &sdl.Rect{X: DisplayMode.W / 2, Y: 0, W: b.WhitePawn.size.W, H: b.WhitePawn.size.H})
+	for i := 0; i < 19; i++ {
+		for j := 0; j < 19; j++ {
+			currVal := arena.Gomoku.Arena.Goban[i+j*19]
+			if currVal == 1 {
+				Renderer.Copy(b.WhitePawn.texture, &b.WhitePawn.size,
+					&sdl.Rect{
+						X: b.Table.pos.X + 12 + b.WhitePawn.pos.W*int32(i),
+						Y: b.Table.pos.Y + 12 + b.WhitePawn.pos.H*int32(j),
+						W: b.WhitePawn.pos.W - 10,
+						H: b.WhitePawn.pos.H - 10,
+					})
+			} else {
+				Renderer.Copy(b.BlackPawn.texture, &b.BlackPawn.size,
+					&sdl.Rect{
+						X: b.Table.pos.X + 12 + b.BlackPawn.pos.W*int32(i),
+						Y: b.Table.pos.Y + 12 + b.BlackPawn.pos.H*int32(j),
+						W: b.BlackPawn.pos.W - 10,
+						H: b.BlackPawn.pos.H - 10,
+					})
+			}
+		}
 	}
+
+	//    demoArena := arena.Gomoku.Arena
+	// for idx, _ := range demoArena.Goban {
+
+	// 	Renderer.Copy(b.WhitePawn.texture, &b.WhitePawn.size, &sdl.Rect{X: b.Table.pos.X + b.WhitePawn.pos.X*int32(idx),
+	// 		Y: b.Table.pos.Y + b.WhitePawn.pos.Y*int32(idx), W: b.WhitePawn.pos.W, H: b.WhitePawn.pos.H})
+	// }
 	Renderer.Present()
 
 }
