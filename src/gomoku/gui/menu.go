86de987cefef40e058765295a743248b0dd1acfd
Vincent Letarouilly <vletarou@student.42.fr>
2015-05-19 16:10:07 +0200

    Can click on Quit

--- src/gomoku/gui/menu.go
+++ src/gomoku/gui/menu.go
@@ -9,13 +9,23 @@ type MenuMain struct {
 	Background *Text
 	Font       *ttf.Font
 	Title      *Text
+	Play       *Text
+	Settings   *Text
+	Quit       *Text
 }
 
 func NewMainMenu() *MenuMain {
 	return &MenuMain{
 		Background: GetTextureFromImage("data/img/bg.jpg"),
 		Title:      GetTextureFromFont("data/fonts/TaiLeb.ttf", "Gogomoku", 150, sdl.Color{R: 255, G: 255, B: 255, A: 255}),
+		Play:       GetTextureFromFont("data/fonts/TaiLeb.ttf", "Play", 150, sdl.Color{R: 0, G: 255, B: 255, A: 255}),
+		Settings:   GetTextureFromFont("data/fonts/TaiLeb.ttf", "Settings", 150, sdl.Color{R: 0, G: 255, B: 255, A: 255}),
+		Quit:       GetTextureFromFont("data/fonts/TaiLeb.ttf", "Quit", 150, sdl.Color{R: 0, G: 255, B: 255, A: 255}),
 	}
+}
+
+func XYInRect(rect sdl.Rect, x int32, y int32) bool {
+	return ((x > rect.X && x < rect.X+rect.W) && (y > rect.Y && y < rect.Y+rect.H))
 }
 
 func (s *MenuMain) PlayScene() {
@@ -27,6 +37,13 @@ func (s *MenuMain) PlayScene() {
 			if t.Keysym.Sym == sdl.K_ESCAPE {
 				Running = false
 			}
+		case *sdl.MouseButtonEvent:
+			if t.Button == sdl.BUTTON_LEFT {
+				if XYInRect(sdl.Rect{X: 2560/2 - s.Quit.size.W/2, Y: (1440 / 7) * 5, W: s.Quit.size.W, H: s.Quit.size.H}, t.X, t.Y) {
+					Running = false
+				}
+
+			}
 		}
 	}
 	Renderer.Clear()
@@ -34,7 +51,10 @@ func (s *MenuMain) PlayScene() {
 	Renderer.FillRect(&sdl.Rect{X: 0, Y: 0, W: 2560, H: 1440})
 
 	Renderer.Copy(s.Background.texture, &s.Background.size, &s.Background.size)
-	Renderer.Copy(s.Title.texture, &s.Title.size, &sdl.Rect{X: 2560/2 - s.Title.size.W/2, Y: 1440 / 8, W: s.Title.size.W, H: s.Title.size.H})
+	Renderer.Copy(s.Title.texture, &s.Title.size, &sdl.Rect{X: 2560/2 - s.Title.size.W/2, Y: 1440 / 7, W: s.Title.size.W, H: s.Title.size.H})
+	Renderer.Copy(s.Play.texture, &s.Play.size, &sdl.Rect{X: 2560/2 - s.Play.size.W/2, Y: (1440 / 7) * 3, W: s.Play.size.W, H: s.Play.size.H})
+	Renderer.Copy(s.Settings.texture, &s.Settings.size, &sdl.Rect{X: 2560/2 - s.Settings.size.W/2, Y: (1440 / 7) * 4, W: s.Settings.size.W, H: s.Settings.size.H})
+	Renderer.Copy(s.Quit.texture, &s.Quit.size, &sdl.Rect{X: 2560/2 - s.Quit.size.W/2, Y: (1440 / 7) * 5, W: s.Quit.size.W, H: s.Quit.size.H})
 
 	Renderer.Present()
 }
