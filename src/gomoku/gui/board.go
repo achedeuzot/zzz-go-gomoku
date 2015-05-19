4791caaf6f37433c8746f05fb54d3804acbadf33
Vincent Letarouilly <vletarou@student.42.fr>
2015-05-19 19:08:34 +0200

    Display Pawns on mouse position

--- src/gomoku/gui/board.go
+++ src/gomoku/gui/board.go
@@ -2,6 +2,7 @@ package gui
 
 import (
 	"gomoku/arena"
+	"log"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
@@ -10,6 +11,7 @@ type Board struct {
 	Background *Texture
 	Table      *Texture
 	Pawns      []*Texture
+	CellSize   sdl.Rect
 }
 
 func NewBoard() *Board {
@@ -39,12 +41,32 @@ func NewBoard() *Board {
 	board.Table.pos = sdl.Rect{X: DisplayMode.W/2 - finalW/2, Y: 0, W: finalW, H: finalH}
 	board.Pawns[arena.WhitePlayer] = GetTextureFromImage("data/img/white.png")
 	board.Pawns[arena.BlackPlayer] = GetTextureFromImage("data/img/black.png")
-	board.Pawns[arena.WhitePlayer].pos = sdl.Rect{X: 0, Y: 0, W: board.Table.pos.W / 19, H: board.Table.pos.H / 19}
-	board.Pawns[arena.BlackPlayer].pos = sdl.Rect{X: 0, Y: 0, W: board.Table.pos.W / 19, H: board.Table.pos.H / 19}
+	board.CellSize = sdl.Rect{X: 0, Y: 0, W: board.Table.pos.W / 19, H: board.Table.pos.H / 19}
+	board.Pawns[arena.WhitePlayer].pos = board.CellSize
+	board.Pawns[arena.BlackPlayer].pos = board.CellSize
 	return board
 }
 
+func (b *Board) XYInCell(x int32, y int32) (int, int) {
+	for i := 0; i < 19; i++ {
+		for j := 0; j < 19; j++ {
+			if x >= b.Table.pos.X+12+b.CellSize.W*int32(i) && x < b.Table.pos.X+12+b.CellSize.W*int32(i+1) &&
+				y >= b.Table.pos.Y+12+b.CellSize.H*int32(j) && y < b.Table.pos.Y+12+b.CellSize.H*int32(j+1) {
+				log.Printf("%d | %d\n", i, j)
+				return i, j
+			}
+		}
+	}
+	log.Println("error")
+	return -1, -1
+}
+
 func (b *Board) PlayScene() {
+	Renderer.Clear()
+
+	Renderer.Copy(b.Background.texture, &b.Background.size, &b.Background.pos)
+	Renderer.Copy(b.Table.texture, &b.Table.size, &b.Table.pos)
+
 	for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 		switch t := event.(type) {
 		case *sdl.QuitEvent:
@@ -53,12 +75,19 @@ func (b *Board) PlayScene() {
 			if t.Keysym.Sym == sdl.K_ESCAPE {
 				CurrScene = SceneMap["MenuMain"]
 			}
+		case *sdl.MouseMotionEvent:
+			log.Printf("%+v\n", t)
+			if i, j := b.XYInCell(t.X, t.Y); i > 0 && j > 0 {
+				Renderer.Copy(b.Pawns[arena.WhitePlayer].texture, &b.Pawns[arena.WhitePlayer].size,
+					&sdl.Rect{
+						X: b.Table.pos.X + 12 + b.Pawns[arena.WhitePlayer].pos.W*int32(i),
+						Y: b.Table.pos.Y + 12 + b.Pawns[arena.WhitePlayer].pos.H*int32(j),
+						W: b.Pawns[arena.WhitePlayer].pos.W - 10,
+						H: b.Pawns[arena.WhitePlayer].pos.H - 10,
+					})
+			}
 		}
 	}
-	Renderer.Clear()
-
-	Renderer.Copy(b.Background.texture, &b.Background.size, &b.Background.pos)
-	Renderer.Copy(b.Table.texture, &b.Table.size, &b.Table.pos)
 
 	// Display content of board in top of background
 	for i := 0; i < 19; i++ {
