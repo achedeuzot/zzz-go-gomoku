085649060270d892636f4bcbb7d8c29ca2230f8a
Vincent Letarouilly <vletarou@student.42.fr>
2015-05-21 15:16:41 +0200

    Fixed capture

--- src/gomoku/arena/goban.go
+++ src/gomoku/arena/goban.go
@@ -27,8 +27,8 @@ func (goban *Goban) Capture(row int32, col int32) {
 	currentColor := goban.GetElem(row, col)
 	opponentColor := getOpponentColor(goban.GetElem(row, col))
 	if goban.canCaptureUp(row, col, currentColor, opponentColor) {
-		goban.SetElem(row-1, col, currentColor)
-		goban.SetElem(row-2, col, currentColor)
+		goban.SetElem(row-1, col, 0)
+		goban.SetElem(row-2, col, 0)
 	}
 }
 
